f69058af468fa73d4cd52799c4604c0bfc20d0bf
daekunhan <dosemfb0920@gmail.com>
2021-09-17 04:01:38 +0900

    # Caching Strategy 변경

--- src/main/java/teamc/opgg/swoomi/config/OriannaConfig.java
+++ src/main/java/teamc/opgg/swoomi/config/OriannaConfig.java
@@ -1,13 +1,14 @@
 package teamc.opgg.swoomi.config;
 
 import com.merakianalytics.orianna.Orianna;
+import com.merakianalytics.orianna.datapipeline.common.expiration.ExpirationPeriod;
 import com.merakianalytics.orianna.types.common.Platform;
 import com.merakianalytics.orianna.types.common.Region;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
 import javax.annotation.PostConstruct;
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class OriannaConfig {
@@ -20,5 +21,8 @@ public class OriannaConfig {
         Orianna.setRiotAPIKey(RIOT_API_KEY);
         Orianna.setDefaultPlatform(Platform.KOREA);
         Orianna.setDefaultRegion(Region.KOREA);
+
+        Orianna.Configuration config = new Orianna.Configuration();
+        config.setCurrentVersionExpiration(ExpirationPeriod.create(0, TimeUnit.SECONDS));
     }
 }

9a6c1001a3e8bf7e313f6af326655db9188248c1
daekun0920 <dosemfb0920@gmail.com>
2021-12-11 15:46:06 +0900

    # server health-check api

--- src/main/java/teamc/opgg/swoomi/controller/EmailTestController.java
+++ src/main/java/teamc/opgg/swoomi/controller/EmailTestController.java
@@ -9,11 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 import teamc.opgg.swoomi.advice.exception.CEmailSendException;
 
 @RestController
-@RequestMapping("/Email")
+@RequestMapping("/email")
 @Api(tags = {"8. Email"})
-public class emailTestController {
+public class EmailTestController {
 
-    @PostMapping("/Test")
+    @PostMapping("/test")
     @ApiOperation(value = "(주의) 이메일이 전송됩니다.", notes = "에러 발생시 이메일 전송 테스트")
     public void sendEmailTest() {
         throw new CEmailSendException();

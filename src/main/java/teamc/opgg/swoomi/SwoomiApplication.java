38247b9735e9e67164a7bcf725bcb8710b86a9f1
daekun0920 <dosemfb0920@gmail.com>
2022-01-07 16:29:42 +0900

    # 기존 불필요한 로직 제거

--- src/main/java/teamc/opgg/swoomi/SwoomiApplication.java
+++ src/main/java/teamc/opgg/swoomi/SwoomiApplication.java
@@ -1,10 +1,7 @@
 package teamc.opgg.swoomi;
 
-import com.merakianalytics.orianna.Orianna;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
